8b29a0dbc78154189d5855f05a20f9cf1e036da2
Amit Sagtani <asagtani06@gmail.com>
2017-06-11 17:58:33 +0530

    Times_Of_India_Headlines_Notifier

---
+++ Times_of_india_news/main.py
@@ -0,0 +1,22 @@
+# Importing important modules required in making of News Machine
+import os
+import bs4
+from bs4 import BeautifulSoup
+import time
+import requests
+import notify2
+
+#Infinite loop for unlimited news :p
+notify2.init("News")
+while True:
+	page = requests.get("http://timesofindia.indiatimes.com/")
+	soup = BeautifulSoup(page.content, "html.parser")
+	headlines = soup.find(id="lateststories")
+	topstories = headlines.find_all(class_="list9")
+	for x in range(len(topstories[0].find_all("a"))):
+		news = (topstories[0].find_all("a")[x]).get_text().strip()
+		display = notify2.Notification(news)
+		display.show()
+		time.sleep(12)
+
+
