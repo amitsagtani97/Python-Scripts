36835662e0fa36a63ff85d0544d1a8fe168d7abb
Matt Graham <matthew.graham@gmail.com>
2017-10-15 15:39:12 -0400

    This turns the while loop into a function that will allow the user to choose between the NY Times and the India Times

--- News Notifier/main.py
+++ News Notifier/main.py
@@ -9,26 +9,35 @@ import notify2
 
 #Infinite loop for unlimited news :p
 notify2.init("News")
-##while True:
-##	page = requests.get("http://timesofindia.indiatimes.com/")
-##	soup = BeautifulSoup(page.content, "html.parser")
-##	headlines = soup.find(id="lateststories")
-##	topstories = headlines.find_all(class_="list9")
-##	for x in range(len(topstories[0].find_all("a"))):
-##		news = (topstories[0].find_all("a")[x]).get_text().strip()
-##		display = notify2.Notification(news)
-##		display.show()
-##		time.sleep(12)
 
-while True:
-	page = requests.get("https://www.nytimes.com/section/world")
-	soup = BeautifulSoup(page.content, "html.parser")
-	headlines = soup.find(id="latest-panel")
-	topstories = headlines.find_all("h2")
-	print(topstories)
-##	for x in range(len(topstories[0].find_all("h2"))):
-##		news = (topstories[0].find_all("h2")[x]).get_text().strip()
-##		display = notify2.Notification(news)
-##		print(news)
-##		display.show()
-##		#time.sleep(12)
+
+#This function looks for a user answer of ny or india and then returns the headlines for that news service. 
+def newsreader(source):
+	if source == 'india':
+		india_page = requests.get("http://timesofindia.indiatimes.com/")
+		india_soup = BeautifulSoup(india_page.content, "html.parser")
+		india_headlines = india_soup.find(id="lateststories")
+		india_topstories = india_headlines.find_all(class_="list9")
+		for x in range(len(india_topstories[0].find_all("a"))):
+				news = (india_topstories[0].find_all("a")[x]).get_text().strip()
+				display = notify2.Notification(news)
+				display.show()
+				time.sleep(12)
+	elif source == 'ny':
+		ny_page = requests.get("https://www.nytimes.com/section/world")
+		ny_soup = BeautifulSoup(ny_page.content, "html.parser")
+		ny_headlines = ny_soup.find(id="latest-panel")
+		for x in ny_headlines.find_all("h2"):
+				news = x.text.strip()
+				display = notify2.Notification(news)
+				display.show()
+				time.sleep(12)
+                
+
+#Takes a users answer and grabs the headlines from the respective news service
+answer = input("Please type NY for New York Times or India for India Times: ").lower()
+while answer !='india' and answer != 'ny':
+        answer = input("Please type NY for New York Times or India for India Times: ").lower()
+        
+newsreader(answer)
+	
